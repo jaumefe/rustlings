6d641ac02827d4f249623165ac439ee2215e809a
jaumefe <jaumef.escrig96@gmail.com>
2025-06-08 12:27:18 +0200

    Move semantics problems solved

--- exercises/06_move_semantics/move_semantics2.rs
+++ exercises/06_move_semantics/move_semantics2.rs
@@ -8,6 +8,12 @@ fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
 
 fn main() {
     // You can optionally experiment here.
+    let vec0 = vec![22, 44, 66];
+
+    let vec1 = fill_vec(vec0.clone());
+
+    assert_eq!(vec0, [22, 44, 66]);
+    assert_eq!(vec1, [22, 44, 66, 88]);
 }
 
 #[cfg(test)]
--- exercises/06_move_semantics/move_semantics4.rs
+++ exercises/06_move_semantics/move_semantics4.rs
@@ -1,5 +1,11 @@
 fn main() {
     // You can optionally experiment here.
+    let mut x = Vec::new();
+    let y = &mut x;
+    y.push(42);
+    let z = &mut x;
+    z.push(13);
+    assert_eq!(x, [42, 13]);
 }
 
 #[cfg(test)]
